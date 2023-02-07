ca1e415757536ea1d66df5c3f6b46352ff70404e
Manhattan1984 <mikkimanhattan@gmail.com>
2023-02-07 13:29:10 +0100

    try again

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,4 @@
-import Link from "next/link";
 import React from "react";
-import SideBar from "./(components)/SideBar";
 import SignInNavbar from "./(components)/SignInNavbar";
 
 export default function DashboardLayout({
