d990d02a668ba6507e0065bbdeca0c245dba5d10
manhattan1984 <mikkimanhattan@gmail.com>
2023-02-23 15:42:06 +0100

    chatbot

--- app/head.tsx
+++ app/head.tsx
@@ -1,3 +1,5 @@
+import Script from "next/script";
+
 export default function Head() {
   return (
     <>
@@ -5,6 +7,7 @@ export default function Head() {
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="WealthAid Mining" />
       <link rel="icon" href="/mining_white.ico" />
+      <Script src="//code.tidio.co/gkewlucotrnh7qrgsqnw8tmysw868wty.js" async strategy="lazyOnload" />
     </>
-  )
+  );
 }
