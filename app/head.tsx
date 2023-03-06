ad64acc026f2ccb9a3c67af51b4312756ecf7b40
manhattan1984 <mikkimanhattan@gmail.com>
2023-03-06 23:09:57 +0100

    jivo

--- app/head.tsx
+++ app/head.tsx
@@ -8,19 +8,7 @@ export default function Head() {
       <meta name="description" content="WealthAid Mining" />
       <link rel="icon" href="/mining_white.ico" />
       <Script
-        dangerouslySetInnerHTML={{
-          __html: `
-        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/640359b731ebfa0fe7f0a7a1/1gqmhcup5';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
-        `,
-        }}
+        src="//code.jivosite.com/widget/63IJl5Ckr1"
         async
         strategy="lazyOnload"
       />
