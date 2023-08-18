20d1be602825c3c6f56448e1024268a2b2440414
manhattan1984 <mikkimanhattan@gmail.com>
2023-08-18 10:59:25 +0100

    new smartupps

--- app/head.tsx
+++ app/head.tsx
@@ -15,7 +15,8 @@ export default function Head() {
       <Script>
         {`
 var _smartsupp = _smartsupp || {};
-_smartsupp.key = 'b5501667bb9f2ade13c6bd7f1c094984c8345624';
+_smartsupp.key = '74eea03d63e8d643c3c138059617dea2db0f13a0';
+window.smartsupp||(function(d) {
   var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
   s=d.getElementsByTagName('script')[0];c=d.createElement('script');
   c.type='text/javascript';c.charset='utf-8';c.async=true;
